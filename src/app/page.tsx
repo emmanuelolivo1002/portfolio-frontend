d1da07810ace87c2c249dbf64976e329082e6a19
Emmanuel Olivo <emmanuel@wealthcharts.com>
2024-05-29 12:01:43 -0500

    Update font and connect to strapi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-import Image from "next/image";
+async function getStrapiData(path: string) {
+  const baseUrl = "http://localhost:1337";
+  try {
+    const response = await fetch(baseUrl + path);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-export default function Home() {
+export default async function Home() {
+  const strapiData = await getStrapiData("/api/home-page");
+
+  const { title, description } = strapiData.data.attributes;
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Another Test</p>
+    <main className="container mx-auto py-6">
+      <h1 className="text-5xl font-bold">{title}</h1>
+      <p className="text-xl mt-4">{description}</p>
     </main>
   );
 }
