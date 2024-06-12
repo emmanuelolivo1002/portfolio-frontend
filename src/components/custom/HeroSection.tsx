39117010f928d9f1ea921ba3e198e06398b92960
Emmanuel Olivo <emmanuel@wealthcharts.com>
2024-06-12 15:56:45 -0500

    Update home buttons

--- src/components/custom/HeroSection.tsx
+++ src/components/custom/HeroSection.tsx
@@ -11,10 +11,10 @@ const HeroSection = ({ data }: { readonly data: any }) => {
         <div className="relative z-10 flex flex-col justify-center h-full ">
           <h1 className="text-xl font-bold md:text-5xl lg:text-5xl">{heading}</h1>
           <div className="flex mt-8 gap-8">
-            <Button size="lg" asChild>
+            <Button size="xl" asChild>
               <Link href={primaryLink.url}>{primaryLink.label}</Link>
             </Button>
-            <Button size="lg" asChild variant="outline">
+            <Button size="xl" asChild variant="outline">
               <Link href={secondaryLink.url}>{secondaryLink.label}</Link>
             </Button>
           </div>
