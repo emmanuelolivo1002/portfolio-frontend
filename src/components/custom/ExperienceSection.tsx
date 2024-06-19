eeb4df640ae808d01df2f398ace9c7e2a344edfc
Emmanuel Olivo <emmanuel@wealthcharts.com>
2024-06-19 13:09:17 -0500

    format Experience section

--- src/components/custom/ExperienceSection.tsx
+++ src/components/custom/ExperienceSection.tsx
@@ -95,24 +95,26 @@ const ExperienceSection = ({
   const { title, subtitle, job: jobsArray } = data;
 
   return (
-    <section id="experience" className="container mx-auto">
-      <div className="mb-12 flex flex-col items-center md:mb-24">
-        <h2 className="mb-8 text-center text-5xl text-primary md:text-8xl">
-          {title}
-        </h2>
-        <p className="max-w-prose text-center text-muted-foreground md:text-xl">
-          {subtitle}
-        </p>
-      </div>
-      {/* Timeline */}
-      <div className="relative grid">
-        {/* Absolutely positioned vertical line */}
-        <div className="absolute left-0 top-0 h-full w-[4px] -translate-x-1/2 transform bg-primary lg:left-1/2"></div>
+    <section id="experience" className="bg-background">
+      <div className="container mx-auto">
+        <div className="mb-12 flex flex-col items-center md:mb-24">
+          <h2 className="mb-8 text-center text-5xl text-primary md:text-8xl">
+            {title}
+          </h2>
+          <p className="max-w-prose text-center text-muted-foreground md:text-xl">
+            {subtitle}
+          </p>
+        </div>
+        {/* Timeline */}
+        <div className="relative grid">
+          {/* Absolutely positioned vertical line */}
+          <div className="absolute left-0 top-0 h-full w-[4px] -translate-x-1/2 transform bg-primary lg:left-1/2"></div>
 
-        {/* Cards */}
-        {jobsArray.map((job: Job, i: number) => (
-          <TimelineCard key={job.id} index={i} job={job} />
-        ))}
+          {/* Cards */}
+          {jobsArray.map((job: Job, i: number) => (
+            <TimelineCard key={job.id} index={i} job={job} />
+          ))}
+        </div>
       </div>
     </section>
   );
