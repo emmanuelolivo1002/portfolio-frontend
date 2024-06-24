39ef4e4212f742b12e7f048dcd6658dae14c1ae0
Emmanuel Olivo <emmanuelo1002@gmail.com>
2024-06-24 13:48:56 -0500

    Update navbar

--- src/components/custom/HeaderNav.tsx
+++ src/components/custom/HeaderNav.tsx
@@ -19,50 +19,10 @@ interface HeaderProps {
   };
 }
 
-const useHash = () => {
-  const [hash, setHash] = useState(() => window.location.hash);
-
-  useEffect(() => {
-    const handleHashChange = () => {
-      setHash(window.location.hash);
-    };
-
-    // Add event listener for hash changes
-    window.addEventListener("hashchange", handleHashChange);
-
-    // Cleanup event listener on component unmount
-    return () => {
-      window.removeEventListener("hashchange", handleHashChange);
-    };
-  }, []);
-
-  return hash;
-};
-
 const HeaderNav = ({ data }: Readonly<HeaderProps>) => {
   const { navigationLink } = data;
 
-  const [currentHash, setCurrentHash] = useState("");
-
-  useEffect(() => {
-    const handleHashChange = () => {
-      setCurrentHash(window.location.hash);
-    };
-
-    // Set the initial hash
-    setCurrentHash(window.location.hash);
-
-    // Add event listener for hash changes
-    window.addEventListener("hashchange", handleHashChange);
-
-    // Cleanup event listener on component unmount
-    return () => {
-      window.removeEventListener("hashchange", handleHashChange);
-    };
-  }, []);
-
   const handleClick = (hash: any) => {
-    setCurrentHash(hash);
     window.location.hash = hash;
   };
 
@@ -71,17 +31,12 @@ const HeaderNav = ({ data }: Readonly<HeaderProps>) => {
       <NavigationMenu className="border-b-2 border-ring bg-background/95 px-2 backdrop-blur supports-[backdrop-filter]:bg-background/60 sm:rounded-2xl sm:border-2 sm:px-6">
         <NavigationMenuList className="flex gap-1 overflow-x-auto sm:gap-4">
           {navigationLink.map((link, index) => {
-            const isActive = currentHash === link.url;
             return (
               <NavigationMenuItem key={index}>
                 <Link href={link.url} passHref legacyBehavior>
                   <NavigationMenuLink
                     onClick={() => handleClick(link.url)}
-                    className={`block whitespace-nowrap p-2 text-xs hover:bg-primary/60 hover:text-foreground focus:bg-primary focus:font-bold focus:text-primary-foreground active:bg-primary active:font-bold active:text-primary-foreground sm:text-base ${
-                      isActive
-                        ? "bg-primary font-bold text-primary-foreground"
-                        : ""
-                    }`}
+                    className={`block whitespace-nowrap p-2 text-xs hover:bg-primary/60 hover:text-foreground focus:bg-primary focus:font-bold focus:text-primary-foreground active:bg-primary active:font-bold active:text-primary-foreground sm:text-base`}
                   >
                     {link.label}
                   </NavigationMenuLink>
