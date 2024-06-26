9b38bff3f8210e71ca49c0d5035819904672b73d
Emmanuel Olivo <emmanuelo1002@gmail.com>
2024-06-26 10:23:05 -0500

    Project Section initial animation and folder structure update

--- src/components/custom/ProjectsSection/ProjectCard.tsx
+++ src/components/custom/ProjectsSection/ProjectCard.tsx
@@ -1,8 +1,11 @@
+"use client";
+
+import { useState } from "react";
+
 // Components
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import ProjectCardWrapper from "./ProjectCardWrapper";
 import TechnologyIconRenderer from "../TechnologyIconRenderer";
+import ProjectDialog from "./ProjectDialog";
 
 // utils
 import { getStrapiURL } from "@/lib/utils";
@@ -11,6 +14,8 @@ import { getStrapiURL } from "@/lib/utils";
 import { Project } from "@/types/projectTypes";
 
 const ProjectCard = ({ project }: { project: Project }) => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
   const {
     id,
     title,
@@ -25,64 +30,78 @@ const ProjectCard = ({ project }: { project: Project }) => {
   // Get at most 10 technologies
   const technologiesToShow = technologies.slice(0, 6);
 
+  const handleOpenDialog = () => {
+    setIsDialogOpen(true);
+  };
+
   return (
-    <ProjectCardWrapper projectId={id}>
-      {/* Image */}
-      <div className="relative mb-4 aspect-video w-full overflow-hidden rounded-lg [&>div]:hover:opacity-100 [&_img]:hover:scale-105">
-        {/* Add a overlay with a search icon in the center */}
-        <div className="absolute inset-0 z-10 flex items-center justify-center bg-black bg-opacity-30 opacity-0 transition-opacity duration-300 ease-out">
-          <div className="flex items-center justify-center rounded-full border-2 border-muted-foreground/10 bg-muted p-2 shadow-xl">
-            <span className="iconify text-5xl text-primary mdi--search"></span>
-          </div>
-        </div>
-        <Image
-          src={getStrapiURL() + thumbnailData.url}
-          alt={thumbnail.name}
-          className="h-full w-full object-cover object-center transition-transform duration-300 ease-out"
-          width={thumbnailData.width}
-          height={thumbnailData.height}
-        />
-      </div>
-
-      {/* Category Pills */}
-      <div className="flex gap-3">
-        {project_categories.map((category) => (
-          <span
-            key={category.id}
-            className="rounded-2xl border-2 border-muted-foreground px-2 py-1 text-xs text-muted-foreground"
-          >
-            {category.label}
-          </span>
-        ))}
-      </div>
-
-      {/* Project Title */}
-      <h3 className="mt-4 text-3xl font-semibold text-primary hover:underline">
-        {title}
-      </h3>
-
-      {/* Short Description */}
-      <p className="mt-2 text-sm text-foreground md:text-base lg:text-base xl:text-lg">
-        {shortDescription}
-      </p>
-
-      {/* Technologies */}
-      {technologiesToShow.length && (
-        <div className="mt-4 flex gap-2 overflow-hidden">
-          {technologiesToShow.map(({ iconKey }) => (
-            <div
-              key={iconKey}
-              className="bg-background-alt flex items-center justify-center rounded-full border border-secondary-foreground/20 p-2"
-            >
-              <TechnologyIconRenderer
-                iconKey={iconKey}
-                className="text-lg text-muted-foreground"
-              />
+    <>
+      <div
+        onClick={handleOpenDialog}
+        className="flex cursor-pointer flex-col text-foreground"
+      >
+        {/* Image */}
+        <div className="relative mb-4 aspect-video w-full overflow-hidden rounded-lg [&>div]:hover:opacity-100 [&_img]:hover:scale-105">
+          {/* Add a overlay with a search icon in the center */}
+          <div className="absolute inset-0 z-10 flex items-center justify-center bg-black bg-opacity-30 opacity-0 transition-opacity duration-300 ease-out">
+            <div className="flex items-center justify-center rounded-full border-2 border-muted-foreground/10 bg-muted p-2 shadow-xl">
+              <span className="iconify text-5xl text-primary mdi--search"></span>
             </div>
+          </div>
+          <Image
+            src={getStrapiURL() + thumbnailData.url}
+            alt={thumbnail.name}
+            className="h-full w-full object-cover object-center transition-transform duration-300 ease-out"
+            width={thumbnailData.width}
+            height={thumbnailData.height}
+          />
+        </div>
+
+        {/* Category Pills */}
+        <div className="flex gap-3">
+          {project_categories.map((category) => (
+            <span
+              key={category.id}
+              className="rounded-2xl border-2 border-muted-foreground px-2 py-1 text-xs text-muted-foreground"
+            >
+              {category.label}
+            </span>
           ))}
         </div>
-      )}
-    </ProjectCardWrapper>
+
+        {/* Project Title */}
+        <h3 className="mt-4 text-3xl font-semibold text-primary hover:underline">
+          {title}
+        </h3>
+
+        {/* Short Description */}
+        <p className="mt-2 text-sm text-foreground md:text-base lg:text-base xl:text-lg">
+          {shortDescription}
+        </p>
+
+        {/* Technologies */}
+        {technologiesToShow.length && (
+          <div className="mt-4 flex gap-2 overflow-hidden">
+            {technologiesToShow.map(({ iconKey }) => (
+              <div
+                key={iconKey}
+                className="bg-background-alt flex items-center justify-center rounded-full border border-secondary-foreground/20 p-2"
+              >
+                <TechnologyIconRenderer
+                  iconKey={iconKey}
+                  className="text-lg text-muted-foreground"
+                />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+      <ProjectDialog
+        projectId={id}
+        isOpen={isDialogOpen}
+        onOpenChange={setIsDialogOpen}
+      />
+    </>
   );
 };
 
--- src/components/custom/ProjectsSection/ProjectsSection.tsx
+++ src/components/custom/ProjectsSection/ProjectsSection.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { motion } from "framer-motion";
+
 // Components
 import ProjectCard from "./ProjectCard";
 
@@ -19,20 +22,55 @@ const ProjectsSection = ({
     <section id="projects" className="bg-background py-12 md:py-24 lg:py-36">
       <div className="container mx-auto">
         <div className="mb-12 flex flex-col items-center md:mb-20">
-          <h2 className="mb-8 text-center text-5xl text-primary md:text-8xl">
+          <motion.h2
+            className="mb-8 text-center text-5xl text-primary md:text-8xl"
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+              duration: 0.5,
+              delay: 0.2,
+              ease: "easeOut",
+            }}
+            viewport={{
+              amount: "all",
+              once: true,
+            }}
+          >
             {title}
-          </h2>
-          <p className="max-w-prose text-center text-muted-foreground md:text-xl">
+          </motion.h2>
+          <motion.p
+            className="max-w-prose text-center text-muted-foreground md:text-xl"
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+              duration: 0.5,
+              delay: 0.2,
+              ease: "easeOut",
+            }}
+            viewport={{
+              amount: "all",
+              once: true,
+            }}
+          >
             {subtitle}
-          </p>
+          </motion.p>
         </div>
 
         {/* Cards Container */}
-        <div className="grid gap-14 md:grid-cols-2 md:gap-14 xl:grid-cols-3">
+        <motion.div
+          className="grid gap-14 md:grid-cols-2 md:gap-14 xl:grid-cols-3"
+          initial="hidden"
+          animate="visible"
+          transition={{
+            delayChildren: 1.8,
+            staggerChildren: 0.2,
+            ease: "easeInOut",
+          }}
+        >
           {projects.map((project) => (
             <ProjectCard key={project.id} project={project} />
           ))}
-        </div>
+        </motion.div>
       </div>
     </section>
   );
