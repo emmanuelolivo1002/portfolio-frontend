5f0f404dc0681e2e3d9d987d4601ecd8ab4bb560
Emmanuel Olivo <emmanuelo1002@gmail.com>
2024-06-26 20:47:34 -0500

    Project dialog layout update

--- src/components/custom/ProjectsSection/ProjectDialog.tsx
+++ src/components/custom/ProjectsSection/ProjectDialog.tsx
@@ -62,7 +62,7 @@ const ProjectContentRenderer = ({ content }: { content: Project }) => {
   return (
     <div
       ref={contentRef}
-      className="relative min-h-80 space-y-6 overflow-y-auto px-6"
+      className="relative grid min-h-80 gap-6 overflow-y-auto overflow-x-hidden px-6 lg:grid-rows-2 lg:content-start lg:justify-start"
     >
       <motion.div
         className="fixed left-0 right-0 top-0 h-2 origin-left bg-primary"
@@ -71,7 +71,7 @@ const ProjectContentRenderer = ({ content }: { content: Project }) => {
 
       {/* Category Pills */}
       {project_categories.length && (
-        <div className="flex gap-3">
+        <div className="flex gap-3 lg:hidden">
           {project_categories.map((category) => (
             <span
               key={category.id}
@@ -85,44 +85,61 @@ const ProjectContentRenderer = ({ content }: { content: Project }) => {
 
       {/* Article Content  */}
       <ArticleRenderer
-        className="mx-auto max-w-none"
+        className="mx-auto max-w-prose lg:col-start-1 lg:w-[65ch]"
         content={detailedDescription}
       />
 
-      {/* Technologies Card */}
-      {technologies.length && (
-        <div className="bg-background-alt grid grid-cols-3 gap-2 rounded-xl p-4 text-muted-foreground lg:grid-cols-4 lg:p-6">
-          <h3 className="col-span-full mb-2 text-xl">Technologies Used:</h3>
-          {technologies.map(({ label, iconKey }) => (
-            <div key={iconKey} className="flex items-center gap-2">
-              <TechnologyIconRenderer iconKey={iconKey} className="text-lg" />
-              {label}
-            </div>
-          ))}
-        </div>
-      )}
+      {/* Sidebar  */}
+      <div className="flex flex-col gap-4 lg:sticky lg:top-0 lg:col-start-2">
+        {/* Category Pills */}
+        {project_categories.length && (
+          <div className="flex gap-3 max-lg:hidden">
+            {project_categories.map((category) => (
+              <span
+                key={category.id}
+                className="rounded-2xl border-2 border-muted-foreground px-2 py-1 text-xs text-muted-foreground"
+              >
+                {category.label}
+              </span>
+            ))}
+          </div>
+        )}
 
-      {/* Button Links */}
-      {(linkToView || linkToCode) && (
-        <div className="align-center flex flex-col justify-center gap-4 pt-4 md:flex-row md:justify-start">
-          {linkToView && (
-            <Button size="lg" variant="outline" asChild>
-              <a href={linkToView} target="_blank">
-                <span className="iconify mr-2 text-xl mdi--external-link"></span>
-                Go To Live Project
-              </a>
-            </Button>
-          )}
-          {linkToCode && (
-            <Button size="lg" variant="outline" asChild>
-              <a href={linkToCode} target="_blank">
-                <span className="iconify mr-2 text-2xl mdi--code"></span> View
-                Source Code
-              </a>
-            </Button>
-          )}
-        </div>
-      )}
+        {/* Technologies Card */}
+        {technologies.length && (
+          <div className="bg-background-alt grid gap-2 gap-x-8 rounded-xl p-4 text-muted-foreground md:grid-cols-2 lg:p-6">
+            <h3 className="col-span-full mb-2 text-xl">Technologies Used:</h3>
+            {technologies.map(({ label, iconKey }) => (
+              <div key={iconKey} className="flex items-center gap-2">
+                <TechnologyIconRenderer iconKey={iconKey} className="text-lg" />
+                {label}
+              </div>
+            ))}
+          </div>
+        )}
+
+        {/* Button Links */}
+        {(linkToView || linkToCode) && (
+          <div className="align-center flex flex-col flex-wrap justify-center gap-4 md:justify-start">
+            {linkToView && (
+              <Button size="lg" variant="outline" asChild>
+                <a href={linkToView} target="_blank">
+                  <span className="iconify mr-2 text-xl mdi--external-link"></span>
+                  Go To Live Project
+                </a>
+              </Button>
+            )}
+            {linkToCode && (
+              <Button size="lg" variant="outline" asChild>
+                <a href={linkToCode} target="_blank">
+                  <span className="iconify mr-2 text-2xl mdi--code"></span> View
+                  Source Code
+                </a>
+              </Button>
+            )}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
@@ -159,7 +176,7 @@ const ProjectDialog = ({
         onOpenChange(open);
       }}
     >
-      <DialogContent className="max-w-screen flex max-h-[90%] flex-col gap-8 px-0 md:max-w-screen-sm lg:max-w-screen-md xl:max-w-screen-md">
+      <DialogContent className="flex max-h-[90%] w-max max-w-[90%] flex-col gap-8 px-0 lg:max-w-none">
         {content ? (
           <>
             <DialogTitle className="px-6 text-primary">
