eaf66ba80b80f3e665ed0afd7a7fbad99c6c683a
Emmanuel Olivo <emmanuelo1002@gmail.com>
2024-06-28 13:40:35 -0500

    test revalidation

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -5,15 +5,22 @@ export async function GET(request: NextRequest) {
   const secret = request.nextUrl.searchParams.get("secret");
   const path = request.nextUrl.searchParams.get("path") || "/";
 
-  if (secret !== process.env.REVALIDATE_TOKEN) {
+  console.log(
+    `Received revalidation request for path: ${path} with secret: ${secret}`,
+  );
+
+  if (secret !== process.env.MY_SECRET_TOKEN) {
+    console.log("Invalid token");
     return NextResponse.json({ message: "Invalid token" }, { status: 401 });
   }
 
   try {
     // Trigger revalidation
     await revalidatePath(path);
+    console.log(`Successfully revalidated path: ${path}`);
     return NextResponse.json({ revalidated: true });
   } catch (err) {
+    console.error(`Error revalidating path: ${path}`, err);
     return NextResponse.json(
       { message: "Error revalidating" },
       { status: 500 },
