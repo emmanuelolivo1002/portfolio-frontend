892cf0d69e76a41a63c5d7c35f8c2676be8f82b1
Emmanuel Olivo <emmanuelo1002@gmail.com>
2024-06-28 13:58:11 -0500

    Change to revalidate to POST

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,20 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 import { revalidatePath } from "next/cache";
 
-export async function GET(request: NextRequest) {
+export async function POST(request: NextRequest) {
   const token = request.nextUrl.searchParams.get("tk");
   const path = request.nextUrl.searchParams.get("path") || "/";
-
-  console.log(
-    `Received revalidation request for path: ${path} with token: ${token}`,
-  );
-
-  if (token !== process.env.REVALIDATE_TOKEN) {
-    console.log("Invalid token");
-    return NextResponse.json({ message: "Invalid token" }, { status: 401 });
-  }
-
   try {
+    console.log(
+      `Received revalidation request for path: ${path} with token: ${token}`,
+    );
+
+    if (token !== process.env.REVALIDATE_TOKEN) {
+      console.log("Invalid token");
+      return NextResponse.json({ message: "Invalid token" }, { status: 401 });
+    }
+
     // Trigger revalidation
     await revalidatePath(path);
     console.log(`Successfully revalidated path: ${path}`);
