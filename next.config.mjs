6494f45521b659a4e851ecbe9f5249179e715e21
Emmanuel Olivo <emmanuelo1002@gmail.com>
2024-07-01 11:55:16 -0500

    Add adminv2 config

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "worthy-renewal-37fc677982.media.strapiapp.com",
       },
+      {
+        protocol: "https",
+        hostname: "adminv2.emmanuelolivo.com",
+      },
     ],
   },
   webpack(config) {
