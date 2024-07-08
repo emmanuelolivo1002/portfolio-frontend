b2bdf6a4e2aca8ffae6dae13ca864c760cff64ee
Emmanuel Olivo <emmanuelo1002@gmail.com>
2024-07-08 10:44:44 -0500

    Update footer

--- src/components/custom/Footer.tsx
+++ src/components/custom/Footer.tsx
@@ -8,19 +8,19 @@ const Footer = ({ data }: FooterProps) => {
   const { linkToSource } = data;
   return (
     <footer className="border-t border-primary/40 bg-background py-4 text-xs text-muted-foreground lg:text-base">
-      <div className="container mx-auto flex flex-wrap justify-center gap-2 px-6 md:justify-between">
-        <p className="relative flex items-center gap-1">
-          <span className="iconify lucide--copyright relative"></span>Emmanuel
-          Olivo 2024 - Present
+      <div className="container mx-auto flex flex-col flex-wrap items-center justify-center gap-2 px-6 md:flex-row md:justify-between">
+        <p className="flex items-center gap-1">
+          <span className="iconify lucide--copyright"></span>Emmanuel Olivo 2024
+          - Present
         </p>
-        <p className="relative">
+        <p className="flex items-center">
           Made with{" "}
-          <span className="iconify mdi--heart relative top-1 text-base text-red-500 lg:text-lg"></span>{" "}
+          <span className="iconify mx-1 text-base text-red-500 mdi--heart lg:text-lg"></span>{" "}
           and{" "}
           <a
             href={linkToSource}
             target="_blank"
-            className="cursor-pointer underline hover:text-primary"
+            className="ml-1 cursor-pointer underline hover:text-primary"
           >
             Next.js
           </a>
